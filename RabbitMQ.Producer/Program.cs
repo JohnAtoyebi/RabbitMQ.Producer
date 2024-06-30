c27f1eba53485958510221420ce9c530ed314940
Atoyebi John <atoyebi.john@bankly.ng>
2024-06-30 12:35:34 +0100

    Add project files.

---
+++ RabbitMQ.Producer/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using RabbitMQ.Client;
+using System.Text;
+using System.Text.Json;
+
+var factory = new ConnectionFactory
+{
+    Uri = new Uri("amqp://guest:guest@localhost:5672")
+};
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare("demo-queue",
+    durable: true,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+
+var message = new { Name = "Producer", Message = "Hello!" };
+var body = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(message));
+
+channel.BasicPublish("", "demo-queue", null, body);
